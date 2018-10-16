05c9dcea95d100b383f49a2f57deef3bc817651c
Omran Aleid <omran@guarana.com>
2018-10-16 12:39:24 -0400

    Initial Commit

---
+++ ScanQR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScanQR
+//
+//  Created by Omran on 2018-10-16.
+//  Copyright © 2018 Guarana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
